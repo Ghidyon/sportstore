555013691cfcdb888ed37444716c5b935ec3477d
ghidyon <gideonakunana@gmail.com>
2021-11-21 14:42:53 +0100

    refactor: consumed RESTful web service

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
+import { asyncActions } from "./AsyncMiddleware"
 
 // setting up a datastore using a reducer
 export const SportStoreDataStore =
-    createStore(CommonReducer(ShopReducer, CartReducer));
+    createStore(CommonReducer(ShopReducer, CartReducer),
+        applyMiddleware(asyncActions));
